699a84dd79df005e6fa759073c2a7ba26d35901f
PLEDGE-4-YOU <upadhyayji78@gmail.com>
2024-06-13 11:21:03 +0530

    added login api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const register = async (req, res) => {
@@ -16,5 +17,30 @@ const register = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-module.exports = { register };
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { register, login };
